95e815511b83acf6c2f5c99e03308a674edcbfe5
Austin Jackson <au5ton@icloud.com>
2016-05-20 21:10:20 -0500

    App preferences

--- index.js
+++ index.js
@@ -5,8 +5,15 @@
 **
 */
 
+'use strict';
+
+var fs = require('fs');
 
 module.exports.auth = require('./lib/auth.js');
 module.exports.chat = require('./lib/chat.js');
 module.exports.torrent = require('./lib/torrent.js');
 module.exports.song = require('./lib/song.js');
+module.exports.constants = require('./lib/constants.js');
+
+//Creates common preferences folder
+fs.mkdir(module.exports.constants.APP_PREFS);
