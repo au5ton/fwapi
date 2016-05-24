a97f5248f39b00a65f27401ef540a4672c370ac8
Austin Jackson <austinjckson@gmail.com>
2016-05-24 14:03:11 -0500

    woo we actually fixed it?!

--- index.js
+++ index.js
@@ -8,7 +8,13 @@
 'use strict';
 
 
-require('ssl-root-cas/latest').inject()
+require('ssl-root-cas/latest')
+.addFile(__dirname + '/ssl/isrgrootx1.pem')
+.addFile(__dirname + '/ssl/lets-encrypt-x1-cross-signed.pem')
+.addFile(__dirname + '/ssl/lets-encrypt-x2-cross-signed.pem')
+.addFile(__dirname + '/ssl/lets-encrypt-x3-cross-signed.pem')
+.addFile(__dirname + '/ssl/lets-encrypt-x4-cross-signed.pem')
+.inject();
 
 var fs = require('fs');
 
